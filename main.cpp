2a8bd1d18e5fb9ae213fc6084de264db2837ee5d
Evan Wilbur <ewilbur9@gmail.com>
2018-05-31 11:44:16 -0500

    Working towers of hanoi solver
    Optimally solves hanoi problem when numTowers == 3. Solves case of
    n towers, but not optimally yet.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+/* #include <string> */
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
 void zip(vector<string>&, vector<string>*, vector<string>*);
 
-int main() {
-  Hanoi h(9);
+int main(int argc, char **argv) {
+  if ( argc != 2) exit(0);
+  Hanoi h(atoi(argv[1]));
   h.prettyPrint();
   h.solve();
 }
